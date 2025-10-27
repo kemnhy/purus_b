afdebbc72e0185db38b1c0d39efdad5bce377a26
keemnhy <keemnhy@gmail.com>
2025-10-27 11:42:56 +0900

    폴더 생성

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <Quick_btn />
+  </div>
+</template>
+<script setup>
+import Quick_btn from './components/Quick_btn.vue';
+
+</script>
+<style lang="scss" scoped></style>
